8dd899212242756e9bb300a94fd715d27aee4091
nmouquet <nicolas.mouquet@cnrs.fr>
2025-06-02 10:45:52 +0200

    #21

--- website.R
+++ website.R
@@ -16,8 +16,7 @@ if (!require("bskyr")) {
 # Fetch feed
 feed <- bskyr::bs_get_feed('at://did:plc:ppsghcl5bbpgjcljnhra353s/app.bsky.feed.generator/global.ecology',
                            limit=150)
-#feed <- feed[!sapply(feed$embed, is.null),]
-feed <- feed[1:12,]
+feed <- feed[!sapply(feed$uri, is.na),]
 
 nb_post=0
 for (i in 1:nrow(feed)){
